9ce4b3f24b8cf31bafc6ec2ab9d954131fa117c1
umair786582 <160109939+umair786582@users.noreply.github.com>
2024-04-07 16:33:45 +0500

    push my todo list

---
+++ main.ts
@@ -0,0 +1,31 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+let shopping = [];
+let windowShoping = true;
+while (windowShoping){
+console.log ("Welcome in task list");
+
+let addtask = await inquirer.prompt(
+    [
+        {
+            name: "stuff",
+            type: "input",
+            message:"what do you want to add in your task list?"
+        },
+        {
+            name: "addmore_stuff",
+            type: "confirm",
+            message: "do you want to add more in list?",
+            default: false
+      
+        }
+    ]
+);
+
+shopping.push (addtask.stuff)
+windowShoping = addtask.addmore_stuff
+console.log(shopping)
+}
+
